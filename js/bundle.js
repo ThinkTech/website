b0bff972021c9e72db0466c97f7db00caa23da14
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2018-03-07 20:58:35 +0000

    improving the project.

--- js/bundle.js
+++ js/bundle.js
@@ -34,7 +34,7 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
 
 $(document).ready(function() {
 	$("body").css("opacity",1);
-	$(".w3_agile_logo h1").addClass("animated flip delay-1");
+	$(".w3_agile_logo h1").addClass("animated flip");
 	$(".banner h2,.banner p").addClass("animated pulse delay-1");
 	$(".subscribe").click(function(event){
 	   $(".plans .pricing").hide();
