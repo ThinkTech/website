dbe4ce09eea0061cd384b9281dc28831f4b3ffb3
Thinktech <dev@thinktech.sn>
2018-05-09 08:17:48 +0000

    improving the project.

--- js/bundle.js
+++ js/bundle.js
@@ -204,7 +204,7 @@ $(document).ready(function() {
     	  	    	          tbody.append(tr);
     	  	    	    	}else {
     	  	    	    	  tr = $("<tr/>").append("<td>"+domain+"."+property+"<td/>");
-       	  	    	          tr.append("<td>indisponible</td>");
+    	  	    	    	  tr.append("<td>"+pricing[property] +" CFA</td>");
        	  	    	          tr.addClass("unavailable");
        	  	    	          tbody.append(tr);
     	  	    	    	}
