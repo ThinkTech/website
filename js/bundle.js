c3bb3c2d38c1222a67c28b2a340ac64e76c0cb8a
Thinktech <dev@thinktech.sn>
2018-06-05 22:32:45 +0000

    improving the project.

--- js/bundle.js
+++ js/bundle.js
@@ -112,7 +112,7 @@ page.init = function() {
 	});
 	$(".scroll").click(function(event){		
 		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top},300);
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1);
 	});
 };
 
